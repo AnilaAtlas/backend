afe15d77c3f5fa96e96c104d6f5ae11b84b810f1
AnilaAtlas <anila.khan3044@gmail.com>
2025-02-15 05:53:50 +0500

    done

--- Home server/server.mjs
+++ Home server/server.mjs
@@ -1,7 +1,7 @@
 console.log("hello world");
 import express from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 //192.168.1.11:3000
 app.get('/', (req, res) => {
     console.log("Requesting IP: ", req.ip);
@@ -13,3 +13,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
